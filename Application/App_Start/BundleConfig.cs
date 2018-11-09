32378e73f7c9e0fccf2cd5b261500ee9351819cc
roman svitukha <griind@gmail.com>
2018-11-09 01:34:43 -0600

    sv-02 added background and working menu.

--- Application/App_Start/BundleConfig.cs
+++ Application/App_Start/BundleConfig.cs
@@ -11,6 +11,9 @@ namespace Application
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/site").Include(
+                        "~/Scripts/Site.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
---
+++ Application/Scripts/site.js
@@ -0,0 +1,8 @@
+﻿$(function () {
+    var $startBtn = $('#startButton');
+    var $startMenu = $('#startMenu');
+    var $startSubmenu = $('#programsButton');
+    $startBtn.on('click', function () { $startMenu.toggle(); });
+    $startSubmenu.on('click', function () { $('#startSubMenu').toggle(); });
+
+});
