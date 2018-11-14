0b02300395ffe9d2b799aa6e65cf566a5e479b34
roman svitukha <griind@gmail.com>
2018-11-14 01:05:05 -0600

    sv8 added colorpicker

--- Application/Scripts/Site.js
+++ Application/Scripts/Site.js
@@ -5,12 +5,16 @@
     $startBtn.on('click', function () { $startMenu.toggle(); });
     $startSubmenu.on('click', function () { $('#startSubMenu').toggle(); });
 
-    document.getElementById('programs_button').onclick = function () {
+    document.getElementById('paint_button').onclick = function () {
 
         var iframe = document.createElement('iframe');
-        iframe.src = 'http://example.com';
+       
+        iframe.style.position = 'middle';
+
+        iframe.src = 'home/paint';
+        iframe.scrolling = 'no';
         document.body.appendChild(iframe);
-
+       
     };
-
+   
 });
