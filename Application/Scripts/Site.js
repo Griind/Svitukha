70bef4df3a93f96bfb060132508ddbcf067cbec7
roman svitukha <griind@gmail.com>
2018-11-13 23:59:37 -0600

    sv7 - added iframe

---
+++ Application/Scripts/Site.js
@@ -0,0 +1,16 @@
+﻿$(function () {
+    var $startBtn = $('#startButton');
+    var $startMenu = $('#startMenu');
+    var $startSubmenu = $('#programsButton');
+    $startBtn.on('click', function () { $startMenu.toggle(); });
+    $startSubmenu.on('click', function () { $('#startSubMenu').toggle(); });
+
+    document.getElementById('programs_button').onclick = function () {
+
+        var iframe = document.createElement('iframe');
+        iframe.src = 'http://example.com';
+        document.body.appendChild(iframe);
+
+    };
+
+});
